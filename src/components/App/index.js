dce8a9ebab9666451e6c1cea77b5ed4fe551004c
goncalo <goncalo@unknown98fa9bc0e356.lan>
2019-11-26 12:03:08 +0000

    modified paths

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "components/Navbar";
+
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and savessx to reload.
         </p>
         <a
           className="App-link"
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+class Navbar extends React.Component {
+  render() {
+    return <div className="header">ssdas2</div>;
+  }
+}
+
+export default Navbar;
